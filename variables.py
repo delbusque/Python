1958105e99e580ed67e5c64c5da1aeb6055dcc7c
Ventsislav Peychev <del.busque@gmail.com>
2023-06-30 15:18:38 +0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,18 @@
+name = 'Todd'
+num = 100
+
+print('Hello',name.upper()+'.\nHow are you ?')
+print('I`m', num,'% !!!')
+
+flag = name.isupper()
+
+print(flag)
+
+nick = input('nickname: ')
+age = int(input('age: '))
+
+print(nick, 'is', age)
+
+age = 22
+
+print(nick, 'is', age)
