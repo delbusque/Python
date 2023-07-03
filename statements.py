d03bc8072d2740e7059a6662a288f6df1d9287c2
Ventsislav Peychev <del.busque@gmail.com>
2023-07-03 20:07:08 +0300

    Create statements.py

---
+++ statements.py
@@ -0,0 +1,13 @@
+a = 77
+b = 77
+
+if a == b and b == 77:
+    print('Equals')
+elif a == b or b == 77:
+    print('77')
+elif a == 55:
+    print('55')
+elif a > b:
+    print('Greater')
+else:
+    print('Smaller')
