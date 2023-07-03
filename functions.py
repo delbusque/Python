d6ff88bdc734560e1a93efa222235547a8c14628
Ventsislav Peychev <del.busque@gmail.com>
2023-07-03 19:21:52 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,13 @@
-def greeting(name, age):
+def greeting(name, age, num1, num2):
     print('Hello. My name is', name+'. I am', str(age) + '.')
+    return num1 + num2
 
 
 age = input('Enter your age: ')
 
-greeting('Johny', age)
+num1 = int(input('Num1: '))
+num2 = int(input())
+
+num = greeting('Johny', age, num1, num2)
+
+print(num)
