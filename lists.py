b7c5b622c07c9cea6e1ea0fabcbb3abc50779722
delbusque <del.busque@gmail.com>
2023-07-06 13:33:30 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -2,7 +2,7 @@ countries = ['USA', 2, True, 2, 'Ghana', 'Malta']
 cities = list(('Plovdiv', 'Hull'))
 
 cities.append('Mezdra')
-cities.insert(0, 'lemon')
+#cities.insert(0, 'lemon')
 countries.remove(True)
 
 countries.extend(cities)
@@ -17,9 +17,12 @@ print(len(countries))
 
 print(type(countries))
 print(type(countries[-1]))
+
+'''
 print(type(countries[1]))
 print(type(countries[2]))
-
+'''
+ 
 print(countries[4][0])
 print(countries[2:])
 print(countries[1:3])
