a4f8cc17153d2e4db986e55c740cac210ba767c8
delbusque <del.busque@gmail.com>
2023-07-10 17:40:49 +0300

    Create functions.py

---
+++ basics/udemy/functions.py
@@ -0,0 +1,5 @@
+def hello(name='Default'):
+    print(f'Hello {name}')
+
+
+hello('John')
--- basics/udemy/list_comprehension.py
+++ basics/udemy/list_comprehension.py
@@ -2,8 +2,8 @@ new_list = [letter for letter in 'boom']
 
 print(new_list)
 
-evens = [int(x/2) for x in range(0,10,2)]
-odds = [x for x in range(10,20) if x % 2 != 0]
+evens = [int(x/2) for x in range(0, 10, 2)]
+odds = [x for x in range(10, 20) if x % 2 != 0]
 
 print(evens)
 print(odds)
@@ -13,5 +13,5 @@ miles = [round(km * 1.61, 2) for km in kilometers]
 
 print(miles)
 
-list_5 = [x*y for x in [1,2,3] for y in [10,20,30]]
+list_5 = [x*y for x in [1, 2, 3] for y in [10, 20, 30]]
 print(list_5)
