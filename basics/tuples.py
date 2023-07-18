88555a6e023a89b431b0146f5f24473b22863508
Ventsislav Peychev <del.busque@gmail.com>
2023-07-18 13:14:59 +0300

    Unpacking tuples

--- basics/tuples.py
+++ basics/tuples.py
@@ -6,3 +6,21 @@ constr = tuple((1, False, 'Japan', 3.14))
 
 print(constr[3])
 print(len(tup))
+
+def players():
+
+    player_1 = ''
+    while player_1 != 'X' and player_1 != 'O':
+        player_1 = input('Player 1 select "X" or "O": ')
+
+    if player_1 == 'X':
+        player_2 = 'O'
+    else:
+        player_2 = 'X'
+
+    return (player_1, player_2)
+
+player_1, player_2 = players() # unpacking tuple
+
+print(player_1)
+print(player_2)
