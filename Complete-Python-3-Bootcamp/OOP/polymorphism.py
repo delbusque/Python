24acc56e4bed51f3ec4774eaa2654a36f0c9886b
Ventsislav Peychev <del.busque@gmail.com>
2023-07-26 10:57:16 +0300

    Add special __str__ method

--- Complete-Python-3-Bootcamp/OOP/polymorphism.py
+++ Complete-Python-3-Bootcamp/OOP/polymorphism.py
@@ -10,6 +10,9 @@ class Dog(Animal):
 
     def speak(self):
         return f'{self.name} says Bauww'
+    
+    def __str__(self):
+        return f'A dog with name {self.name}'
 
 class Cat(Animal):
 
@@ -18,6 +21,8 @@ class Cat(Animal):
     
 zaks = Dog('Zaks')
 pufi = Cat('Pufi')
+
+print(zaks)
 
 def pet_speaks(pet):
     print(pet.speak())
